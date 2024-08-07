36ba7769c62053c2894a780bfa5059c610c3eb56
Ashish Shah <ashishks@redhat.com>
2024-08-07 15:53:25 +0530

    Update course-init.sh

--- course-init.sh
+++ course-init.sh
@@ -4,3 +4,5 @@ sed -i "s/REPLACEREPONAME/${REPONAME}/g" *.* > /dev/null 2>&1
 ##TODO: Any automation required for repo customization (like for example HOL vs BFX) goes here.
 echo -e "done"
 rm -f $0
+echo -e "\nPlease replace the specified strings in the files below and commit the changes before proceeding with the course development."
+grep REPLACE *.*
