1702e93cabf47ce4e7c1150fcdbaac3b705f9945
Ashish Shah <ashishks@redhat.com>
2024-08-16 19:29:17 +0530

    Changes in course initialization to fix the Report Issue link

--- course-init.sh
+++ course-init.sh
@@ -1,6 +1,7 @@
 REPONAME=$(basename `git rev-parse --show-toplevel`)
 echo -ne "Initializing $REPONAME . . . "
 sed -i "s/REPLACEREPONAME/${REPONAME}/g" *.* > /dev/null 2>&1
+sed -i "s/REPLACEREPONAME/${REPONAME}/g" supplemental-ui/partials/header-content.hbs > /dev/null 2>&1
 ##TODO: Any automation required for repo customization (like for example HOL vs BFX) goes here.
 echo -e "done"
 rm -f $0
